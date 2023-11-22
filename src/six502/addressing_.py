9400a4b5ef52ecee9cb9ce9d71effa2c387b87e9
Manjunath Srinivasa <manjevas@gmail.com>
2023-11-21 22:27:46 -0800

    Adding functionality

--- src/six502/addressing_.py
+++ src/six502/addressing_.py
@@ -15,4 +15,54 @@
 # limitations under the License.
 
 class addressing:
-    pass
+    def __init__(self, bus, registers, clock):
+        self.__bus = bus
+        self.__registers = registers
+        self.__clock = clock
+    
+    def eval(self, mode):
+        if mode == "ACC":
+            self.__clock.tick()
+            return self.__registers["ACC"].get()
+        
+        elif mode =="ABS":
+            self.__clock.tick(2)
+            pc = self.__registers["PC"].get()
+            LL = self.__bus.get(pc)
+            self.__registers["PC"].set(pc + 1)
+            HH = self.__bus.get(pc + 1)
+            self.__registers["PC"].set(pc + 2)
+            return (HH << 8) + LL
+        
+        elif mode =="ABX":
+            self.__clock.tick(2)
+            pc = self.__registers["PC"].get()
+            LL = self.__bus.get(pc)
+            self.__registers["PC"].set(pc + 1)
+            HH = self.__bus.get(pc + 1)
+            self.__registers["PC"].set(pc + 2)
+            X = self.__registers["X"].get()
+            
+            
+            return (HH << 8) + LL + X
+        
+        elif mode == "ABY":
+            pass
+        elif mode == "IMM":
+            pass
+        elif mode == "IMP":
+            pass
+        elif mode == "IND":
+            pass
+        elif mode == "XIN":
+            pass
+        elif mode == "INY":
+            pass
+        elif mode == "REL":
+            pass
+        elif mode == "ZPG":
+            pass
+        elif mode == "ZPX":
+            pass
+        elif mode == "ZPY":
+            pass
